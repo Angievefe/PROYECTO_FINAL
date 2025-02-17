42b491f4dd2afc0ae2dd214ef9aac64cc6be8807
Angievefe <angievegaf111@gmail.com>
2025-02-17 00:25:58 +0000

    PYTHON FINAL

---
+++ main.py
@@ -0,0 +1,6 @@
+from controller.menu import *
+from config.app import *
+if __name__=="__main__":
+    app=App('./PROYECTO_FINAL.db')
+    menu(app)
+    pass
---
+++ modelos/model.py
@@ -0,0 +1,115 @@
+from sqlite3 import Connection
+
+class Pais:
+    """ Tabla id name"""
+    def create_table(self,conn:Connection):
+        query = """
+            CREATE TABLE IF NOT EXISTS PAIS (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(50) NOT NULL
+            );
+            """
+        cursor=conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        
+class PostalCode:
+    """ Tabla CODIGO POSTAL: id, code, pais, ciudad  y estado """ 
+    def create_table(self, con: Connection):
+        query = """
+            CREATE TABLE IF NOT EXISTS POSTALCODE (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                code VARCHAR(50) NOT NULL,
+                pais VARCHAR(50) NOT NULL,
+                state VARCHAR(50) NOT NULL
+            );
+        """
+        cursor = con.cursor()
+        cursor.execute(query)
+        con.commit()        
+
+class Categorias:
+    """ Tabla CATEGORIAS: id, name, subcategory """
+
+    def create_table(self, con: Connection):
+        query = """
+            CREATE TABLE IF NOT EXISTS CATEGORIAS (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(50) NOT NULL,
+                subcategory VARCHAR(50) NOT NULL
+            );
+        """
+        cursor = con.cursor()
+        cursor.execute(query)
+        con.commit()
+
+class Productos:
+    """ Tabla PRODUCTOS: id, name, product_id, subcategory_id """
+
+    def create_table(self, con: Connection):
+        query = """
+            CREATE TABLE IF NOT EXISTS PRODUCTOS (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(50) NOT NULL,
+                product_id INTEGER NOT NULL,
+                category_id INTEGER NOT NULL
+            );
+        """
+        cursor = con.cursor()
+        cursor.execute(query)
+        con.commit()
+
+    class Market:
+        """ Tabla MARKET: id, code, pais, ciudad  y estado """ 
+    def create_table(self, con: Connection):
+        query = """
+            CREATE TABLE IF NOT EXISTS MARKET (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                code VARCHAR(50) NOT NULL,
+                pais VARCHAR(50) NOT NULL,
+                state VARCHAR(50) NOT NULL
+            );
+        """
+        cursor = con.cursor()
+        cursor.execute(query)
+        con.commit()    
+#agregar un clase catalogo
+class Mercado:
+    """ Tabla MERCADO: id, name, product_id, subcategory_id """
+
+    def create_table(self, con: Connection):
+        query = """
+            CREATE TABLE IF NOT EXISTS MERCADO (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(50) NOT NULL,
+                Market_id INTEGER NOT NULL,
+                category_id INTEGER NOT NULL
+            );
+        """
+        cursor = con.cursor()
+        cursor.execute(query)
+        con.commit()
+
+
+class Ventas:
+    """ Tabla VENTAS con múltiples relaciones """
+
+    def create_table(self, con: Connection):
+        query = """
+            CREATE TABLE IF NOT EXISTS VENTAS (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                order_id VARCHAR(20) NOT NULL,
+                postal_code VARCHAR(20),
+                product_id INTEGER NOT NULL,
+                market_id INTEGER NOT NULL,
+                sales_amount REAL NOT NULL,
+                quantity INTEGER NOT NULL,
+                discount REAL NOT NULL,
+                profit REAL NOT NULL,
+                shipping_cost REAL NOT NULL,
+                order_priority VARCHAR(20) NOT NULL
+            );
+        """
+        cursor = con.cursor()
+        cursor.execute(query)
+        con.commit()
